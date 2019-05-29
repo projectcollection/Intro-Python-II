4ba841fe53633b0fda2b067b8040feef67e3266a
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-29 17:15:02 -0400

    implement player class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player():
+
+	def __init__(self, name, current_loc):
+		self.name = name
+		self.current_loc = current_loc
+	
+	def move(new_room):
+		self.current_loc = new_room

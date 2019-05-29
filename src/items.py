0719e07f5440c8c82623947a571f68f60a123a65
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-29 19:04:05 -0400

    add use method to Utility class

--- src/items.py
+++ src/items.py
@@ -5,9 +5,12 @@ class Item():
 		self.value = value
 
 class Utility(Item):
-	def __init__(self, name, weight, value, uses):
+	def __init__(self, name, weight, value, uses = 1):
 		self.uses = uses
 		return super().__init__(name, weight, value)
+	
+	def use(self):
+		self.uses = self.uses - 1;
 
 class Weapon(Item):
 	def __init__(self, name, weight, value, dmg):

6cab4ea50457bb07652380e618c03685244f86c7
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-29 19:02:32 -0400

    change order of args

--- src/items.py
+++ src/items.py
@@ -5,11 +5,11 @@ class Item():
 		self.value = value
 
 class Utility(Item):
-	def __init__(self, name, weight, uses, value):
+	def __init__(self, name, weight, value, uses):
 		self.uses = uses
 		return super().__init__(name, weight, value)
 
 class Weapon(Item):
-	def __init__(self, name, weight, dmg, value):
+	def __init__(self, name, weight, value, dmg):
 		self.dmg = dmg
 		return super().__init__(name, weight, value)
